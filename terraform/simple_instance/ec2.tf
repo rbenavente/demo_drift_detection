e470f475064f0f5b175bc64e2a8080acbfb86341
rbenavente <63422736+rbenavente@users.noreply.github.com>
2022-04-18 14:27:02 +0200

    Update ec2.tf

--- terraform/simple_instance/ec2.tf
+++ terraform/simple_instance/ec2.tf
@@ -12,16 +12,7 @@ resource "aws_security_group" "ssh_traffic" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags = {
-    git_commit           = "1f626f72c4e4055276e6ac5b2e984be2a9bdb25d"
-    git_file             = "terraform/simple_instance/ec2.tf"
-    git_last_modified_at = "2021-11-15 11:41:31"
-    git_last_modified_by = "63422736+rbenavente@users.noreply.github.com"
-    git_modifiers        = "63422736+rbenavente"
-    git_org              = "rbenavente"
-    git_repo             = "demo_drift_detection"
-    yor_trace            = "1fea1b20-ec93-46fe-8e7a-485a1b6fbef2"
-  }
+  
 }
 
 resource "aws_instance" "web_server_instance" {
@@ -29,16 +20,7 @@ resource "aws_instance" "web_server_instance" {
   instance_type   = "t2.micro"
   security_groups = ["${aws_security_group.ssh_traffic.name}"]
 
-  tags = {
-    git_commit           = "1f626f72c4e4055276e6ac5b2e984be2a9bdb25d"
-    git_file             = "terraform/simple_instance/ec2.tf"
-    git_last_modified_at = "2021-11-15 11:41:31"
-    git_last_modified_by = "63422736+rbenavente@users.noreply.github.com"
-    git_modifiers        = "63422736+rbenavente"
-    git_org              = "rbenavente"
-    git_repo             = "demo_drift_detection"
-    yor_trace            = "2b790d6d-94b8-42d0-a6c4-4953d67d593c"
-  }
+  
 }
 
 
